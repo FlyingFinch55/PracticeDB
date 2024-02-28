e7376e5c0d20e84400a55d47d950db5f846746d6
Kimberlee Finch <kmfinch42@tntech.edu>
2024-02-28 09:37:20 -0600

    add blank files

---
+++ z1_createTable.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn= sqlite3.connect('databaseFile.db')
+curse = conn.cursor()
+
+
+curse.execute("""
+              CREATE TABLE IF NOT EXISTS movies (
+              id INTEGER PRIMARY KEY,
+              title TEXT,
+              director TEXT,
+              year INTEGER,
+              gen TEXT )
+              """)
+#Table should be created with table and attrabultes

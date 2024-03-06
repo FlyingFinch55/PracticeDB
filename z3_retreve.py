573b49978f2b785bd5dbfe0f27ec0de833e8820f
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-06 09:08:32 -0600

    fixed spelling mistake

--- z3_retreve.py
+++ z3_retreve.py
@@ -4,5 +4,5 @@ conn= sqlite3.connect('databaseFile.db')
 curse = conn.cursor()
 
 #Reads all table
-curse.execute("SELSECT * FROM movies")
+curse.execute("SELECT * FROM movies")
 print(curse.fetchall())

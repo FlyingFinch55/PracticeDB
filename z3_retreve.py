7ac3924175721b6cd0b7f3fcd61b2ec25ee85275
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-06 09:23:12 -0600

    added print statments to clean up reading

--- z3_retreve.py
+++ z3_retreve.py
@@ -5,20 +5,22 @@ curse = conn.cursor()
 
 curse.execute("SELECT * FROM sqlite_master WHERE type='table'")
 #Reads all table
+print("This gives us the tables in the database")
 print(curse.fetchall())
 print()
 
 #Reads all table
 curse.execute("SELECT * FROM movies")
+print("This gives us everything from the movie table")
 print(curse.fetchall())
 
 #This pulls all colums from table
 curse.execute("PRAGMA table_info({});".format("movies"))
 #this prints eveything about the coloums
 colums =curse.fetchall()
-print(colums)
+print("This is all data about the colums" + colums)
 print()
 
 #This prints just the names
 colsNames = [colum[1] for colum in colums]
-print(colsNames)
+print("This just prints the colums names from the table movies" +colsNames)

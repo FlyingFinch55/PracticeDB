8194f210e50e2bdc89ec054b68faf214d77ea7d9
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-06 09:09:36 -0600

    added code to read all tables from database

--- z3_retreve.py
+++ z3_retreve.py
@@ -3,6 +3,11 @@ import sqlite3
 conn= sqlite3.connect('databaseFile.db')
 curse = conn.cursor()
 
+curse.execute("SELECT * FROM sqlite_master WHERE type='table'")
+#Reads all table
+print(curse.fetchall())
+print()
+
 #Reads all table
 curse.execute("SELECT * FROM movies")
 print(curse.fetchall())

4647876e3e722754fd39b991c27facbf64eeadd5
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-06 09:30:36 -0600

    changes to add more movies

--- z2_PopulateTable.py
+++ z2_PopulateTable.py
@@ -14,3 +14,8 @@ def addMovie(title, director, year, gen):
     print("Movie added :)")
 
 addMovie("StarWars", "John Williams", 1970, "Sci-fi")
+addMovie("Inception", "Christopher Nolan", 2010, "Sci-Fi")
+addMovie("The Shawshank Redemption", "Frank Darabont", 1994, "Drama")
+addMovie("The Godfather", "Francis Ford Coppola", 1972, "Crime")
+addMovie("Pulp Fiction", "Quentin Tarantino", 1994, "Crime")
+addMovie("The Dark Knight", "Christopher Nolan", 2008, "Action")

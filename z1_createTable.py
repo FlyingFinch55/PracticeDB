bea635fca126e56caa50e9d899a95ba0e5ebfda8
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-06 09:07:35 -0600

    made comments

--- z1_createTable.py
+++ z1_createTable.py
@@ -3,7 +3,7 @@ import sqlite3
 conn= sqlite3.connect('databaseFile.db')
 curse = conn.cursor()
 
-
+#movies is table name
 curse.execute("""
               CREATE TABLE IF NOT EXISTS movies (
               id INTEGER PRIMARY KEY,

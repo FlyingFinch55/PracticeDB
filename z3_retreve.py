5b611ee4d09a436c9263e530e3153b5d9ae0f760
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-06 09:21:05 -0600

    Added code to read colums for movies

--- z3_retreve.py
+++ z3_retreve.py
@@ -11,3 +11,14 @@ print()
 #Reads all table
 curse.execute("SELECT * FROM movies")
 print(curse.fetchall())
+
+#This pulls all colums from table
+curse.execute("PRAGMA table_info({});".format("movies"))
+#this prints eveything about the coloums
+colums =curse.fetchall()
+print(colums)
+print()
+
+#This prints just the names
+colsNames = [colum[1] for colum in colums]
+print(colsNames)
